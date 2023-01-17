cde79c7df021dab2cca1e39d75a94ea60f5d4c7a
Peng Li <lichie@qti.qualcomm.com>
2023-01-17 17:02:23 +0800

    修改文件目录结构，并修改使用指南

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import OverviewApp from './overview/App';
 
-import store from './redux/store';
+import store from './overview/redux/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <OverviewApp />
     </Provider>
   </React.StrictMode>
 );
