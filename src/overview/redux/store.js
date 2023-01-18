efe3023c98c9287e6573ffbd4ea4b326e33c65f6
Peng Li <lichie@qti.qualcomm.com>
2023-01-18 09:26:02 +0800

    修改store的说明

--- src/overview/redux/store.js
+++ src/overview/redux/store.js
@@ -5,7 +5,7 @@ import functionReducerSlice from './reducerSlice'
 export default configureStore({
   reducer: {
     //reducer对象的key，是状态的key
-    //这里建立了state的某个属性，与对应的reducer之间的关系
+    //这里建立了state的切片（state的某个属性），与对应的reducer之间的关系
     stateKey: functionReducerSlice
   }
 })
