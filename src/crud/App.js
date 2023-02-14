d5de7cb703380550aed4bd5dac25ec3cf8c024c5
Peng Li <lichie@qti.qualcomm.com>
2023-02-14 16:22:11 +0800

    完善CRUD的UI，以支持skip按钮和update按钮的同时使用

--- src/crud/App.js
+++ src/crud/App.js
@@ -4,22 +4,29 @@ import { endpoints } from './redux/reducerSlice'
 
 import './App.css';
 
+const DataType = {
+  queryData: "q",
+  updateData: "u"
+}
 function App() {
   const [skip, setSkip] = useState(true);
+  const [dataType, setDataType] = useState(true);
 
   const {data, isSuccess, isLoading, isError} = endpoints.getCounter.useQuery(100,{
     // useQuery会在组件挂载完成后，就会开始取数据。
     // 如果需要在指定的时刻再去取数据，需要在初始化的时候，设置skip为true
-    // skip,
+    skip,
     // !!!useQuery中，必须向query方法传递参数，pollingInterval才会生效!!!
-    // pollingInterval: 3000,
+    pollingInterval: 3000,
   });
 
   // useMutation与useQuery不同，在useMutation返回的对象是不同的实例
   // 如果需要useMutation返回相同的实例，需要在两次调用中使用相同的fixedCacheKey
-  const [update, {data: updateData, isSuccess: isUpdateSuccess}] = endpoints.updateCounter.useMutation({
+  const [update, {data: updateData, isUninitialized}] = endpoints.updateCounter.useMutation({
     //fixedCacheKey: 'shared-update-counter',
   });
+
+  const displayData = dataType == DataType.queryData ? data : updateData;
 
   return (
     <div className="App">
@@ -28,19 +35,29 @@ function App() {
           className="App-link"
         >
           {/*Function value: {isUpdateSuccess ? updateData : data}*/}
-          Function value: {data}
+          Function value: {displayData}
         </div>
 
         <button
           style={{marginTop: 50}}
-          onClick={() => setSkip(!skip)}
+          onClick={
+            () => {
+              setDataType(DataType.queryData)
+              setSkip(!skip)
+            }
+          }
         >
           set skip {!skip ? "true" : "false"}
         </button>
 
         <button
             style={{marginTop: 50}}
-            onClick={() => update()}
+            onClick={
+              () => {
+                setDataType(DataType.updateData)
+                update()
+              }
+            }
         >
           Update Counter
         </button>
