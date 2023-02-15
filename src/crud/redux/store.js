04652425bde4da5a582240f0828f94cd73d2e35c
Peng Li <lichie@qti.qualcomm.com>
2023-02-15 17:09:56 +0800

    为createApi添加关于middleware的说明

--- src/crud/redux/store.js
+++ src/crud/redux/store.js
@@ -6,6 +6,7 @@ export default configureStore({
   reducer:{
     [counterApi.reducerPath]: counterApi.reducer
   },
+  //createApi实际上是插入一个中间件，来拦截和转换action
   middleware: getDefaultMiddleware =>
       getDefaultMiddleware().concat(counterApi.middleware)
 });
