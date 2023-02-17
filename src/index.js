cb60a1c65c44d5547fedb86bb4a01288dfbe1148
Peng Li <lichie@qti.qualcomm.com>
2023-02-17 11:24:12 +0800

    增加node版本说明

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './overview/App';
+import App from './overview/App';
 // import App from './async/App';
-import App from './crud/App';
+// import App from './crud/App';
 
-// import store from './overview/redux/store';
+import store from './overview/redux/store';
 // import store from './async/redux/store';
-import store from './crud/redux/store';
+// import store from './crud/redux/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
